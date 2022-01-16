a3a0d8421ca94771cc51e4ad0f81cfc12c8e215a
Kazuma Toyoshima <s20c1079@s.chibakoudai.jp>
2022-01-16 05:17:29 +0000

    add a file

--- scripts/number.py
+++ scripts/number.py
@@ -6,17 +6,26 @@ import rospy
 from std_msgs.msg import Int32
 
 n = 0
+m = 0
 
 def cb(message):
         global n
-        if message.data % 12 == 0:
-            n += 1       
+        n = message.data%12
+
+def cd2(message):
+        global m
+        m = message.data
+
 
 if __name__=='__main__':
-        rospy.init_node('min')
-        sub = rospy.Subscriber('second_up', Int32, cb)
-        pub = rospy.Publisher('min', Int32, queue_size = 1)
-        rate = rospy.Rate(10)
+        rospy.init_node('time')
+        pub = rospy.Publisher('timer1', Int32, queue_size = 1)
+        pub2 = rospy.Publisher('timer2', Int32, queue_size = 1)
+        sub = rospy.Subscriber('second_up',Int32,cb)
+        sub2 = rospy.Subscriber('min',Int32,cd2)
+        rate = rospy.Rate(1)
+
         while not rospy.is_shutdown():
                 pub.publish(n)
+                pub2.publish(m)
                 rate.sleep()
