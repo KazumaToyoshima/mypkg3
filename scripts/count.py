b3f5a215b118773fe527a15047ba4bff5f6b0196
Kazuma Toyoshima <s20c1079@s.chibakoudai.jp>
2022-01-16 05:57:37 +0000

    add a count

---
+++ scripts/count.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+#SPDX-License-Identifier:BSD-2.0
+
+#*Copyright(c)2021 Ryuich Ueda. All rights reserved.
+#*Copyright(c)2021 Kazuma Toyoshima. All rights reserved.
+
+import rospy
+from std_msgs.msg import Int32
+
+
+rospy.init_node('count')
+pub = rospy.Publiser('count_up'), Init32, queue_size=1)
+rate = rospy.Rate(10)
+n = 0
+while not rospy.is_shutdown():
+    n += 1
+    pub.publish(n)
+    rate.sleep()
